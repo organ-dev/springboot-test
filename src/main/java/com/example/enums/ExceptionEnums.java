1abbc61f020c868f96b61be909af8f87a75ebc08
aidon0818 <30116992@qq.com>
2018-09-14 17:53:37 +0800

    统一常见异常枚举

---
+++ src/main/java/com/example/enums/ExceptionEnums.java
@@ -0,0 +1,43 @@
+package com.example.enums;
+
+/**
+ * @Auther: ld
+ * @Date: 2018/9/14 17:44
+ * @Description:
+ */
+public enum ExceptionEnums {
+    EXCEPTION(-1, "系统异常"),
+    METHODARGUMENTNOTVALIDEXCEPTION(-2, "接口数据验证异常"),
+    NULLPOINTEREXCEPTION(-3, "空指针异常类型"),
+    CLASSCASTEXCEPTION(-4, "类型强制转换类型"),
+    ARRAYINDEXOUTOFBOUNDSEXCEPTION(-5, "数组下标越界异常"),
+    FILENOTFOUNDEXCEPTION(-6, "文件未找到异常"),
+    EOFEXCEPTION(-7, "文件已结束异常"),
+    NUMBERFORMATEXCEPTION(-8, "字符串转换为数字异常"),
+    SQLException(-9, "操作数据库异常"),
+    IOException(-10, "操作数据库异常"),
+    NoSuchMethodException(-11, "方法未找到异常"),
+    SystemException(-12, "系统异常"),
+    NegativeArraySizeException(-13, "创建一个大小为负数的数组错误异常"),
+    SecurityException(-14, "安全异常"),
+    UnsupportedOperationException(-15, "不支持的操作异常"),
+    IllegalStateException(-16, "请求状态异常"),
+    InvocationTargetException(-17, "反射异常"),
+    IllegalArgumentException(-18, "非法参数异常"),
+    ;
+    private Integer code;
+    private String msg;
+
+    ExceptionEnums(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
