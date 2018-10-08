85e2b757066b323e78637506b80e0b70d3fe10a4
aidon0818 <30116992@qq.com>
2018-10-08 10:25:56 +0800

    随机数

---
+++ src/main/java/com/example/utils/RandomThread.java
@@ -0,0 +1,23 @@
+package com.example.utils;
+
+/**
+ * @Auther: ld
+ * @Date: 2018/10/8 09:33
+ * @Description:
+ */
+public class RandomThread implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            RandomStringUtil randomStringUtil=new RandomStringUtil();
+            System.out.println(randomStringUtil.randomAlphabetic());
+        }
+    }
+
+    public static void main(String[] args) {
+        RandomThread randomThread = new RandomThread();
+        Thread thread = new Thread(randomThread);
+        thread.start();
+    }
+}
