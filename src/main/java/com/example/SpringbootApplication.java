e08cf8b3edbda3de4ad78b2c5bc7735c3b4a71cf
aidon0818 <30116992@qq.com>
2019-03-20 09:41:20 +0800

    定时任务

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -1,11 +1,15 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
+//@MapperScan("com.example.repository")
+@EnableScheduling
 public class SpringbootApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringbootApplication.class,args);
