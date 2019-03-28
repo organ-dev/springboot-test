c2efa2fb29cb6ef75c7d1632527b5e3a6bfce363
aidon0818 <30116992@qq.com>
2019-03-28 16:09:48 +0800

    定时任务

--- src/main/java/com/example/utils/PrintTask.java
+++ src/main/java/com/example/utils/PrintTask.java
@@ -10,10 +10,12 @@ import java.util.Date;
  * @Date: 2019/3/20 09:27
  * @Param ${tags}
  * @Description: 定时任务
+ * initialDelay 在容器启动后,延迟10秒后再执行
+ * fixedRate 以后每15秒再执行一次该定时器
  */
 @Component
 public class PrintTask {
-@Scheduled(initialDelay = 1000*5,fixedRate = 1000*1)
+@Scheduled(initialDelay = 10000,fixedRate = 15000)
 public void cron() throws Exception{
 //    Thread.sleep(2000);
     System.out.println("执行测试"+new Date(System.currentTimeMillis()));
