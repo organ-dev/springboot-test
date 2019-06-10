729ae96c9ac6ad2292ad77b3ea797afefb66d8eb
tutu <30116992@qq.com>
2019-06-10 14:57:32 +0800

    优化代码:配置

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -1,27 +1,24 @@
 package com.example;
 
-import com.example.utils.seq.RedisSequences;
-import org.mybatis.spring.annotation.MapperScan;
+import com.example.utils.seq.BusinessSeqService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@ComponentScan
+
 @SpringBootApplication
 //@EnableJpaAuditing
 //@MapperScan("com.example.repository")
 //@EnableConfigurationProperties({RedisSequences.class})
-@EnableScheduling
-@EnableCaching
+//@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {BusinessSeqService.class}))//过滤掉不需要的类
+@EnableScheduling//定时器
+@EnableCaching//缓存
 public class SpringbootApplication {
-    public static void main(String[] args){
-        SpringApplication.run(SpringbootApplication.class,args);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
 
-    }
+	}
 }
 
 
