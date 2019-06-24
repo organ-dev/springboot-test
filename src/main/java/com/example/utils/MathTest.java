5b5f671807fe0ab08435df313cb3a043ec625ba4
tutu <30116992@qq.com>
2019-06-24 16:28:04 +0800

    计算汇率值

--- src/main/java/com/example/utils/MathTest.java
+++ src/main/java/com/example/utils/MathTest.java
@@ -1,6 +1,7 @@
 package com.example.utils;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 /**
  * @Auther: ld
@@ -9,14 +10,15 @@ import java.math.BigDecimal;
  * @Description: 计算汇率值
  */
 public class MathTest {
-	public double getValue(String value, String rate) {
-		double val = 0;
+	public String getValue(String value, String rate) {
+		String val = "";
 		try {
 			BigDecimal v = new BigDecimal(value);
 			BigDecimal r = new BigDecimal(rate);
 			BigDecimal result = v.multiply(r).divide(new BigDecimal("100"));
 			BigDecimal one = new BigDecimal("1");
-			val = result.divide(one, 4, BigDecimal.ROUND_HALF_UP).doubleValue();
+			val = result.divide(one, 4, BigDecimal.ROUND_HALF_UP).toString();
+			String decimalFormat=new DecimalFormat("0.0000").format(new Double(val));
 		} catch (Exception e) {
 		}
 		return val;
@@ -24,7 +26,7 @@ public class MathTest {
 
 	public static void main(String[] args) {
 		MathTest mathTest = new MathTest();
-		double val = mathTest.getValue("34.3237", "3.4");
+		String val = mathTest.getValue("34.434", "3.4");
 		System.out.println(val);
 	}
 }
