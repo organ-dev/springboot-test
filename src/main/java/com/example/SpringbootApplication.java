484967f04b56317b23f54db3c85be073fbe3c87e
tutu <30116992@qq.com>
2019-07-03 16:42:01 +0800

    druid 配置

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -1,19 +1,23 @@
 package com.example;
 
-import com.example.utils.seq.BusinessSeqService;
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 //@EnableJpaAuditing
-//@MapperScan("com.example.repository")
+@MapperScan("com.example.repository")
 //@EnableConfigurationProperties({RedisSequences.class})
 //@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {BusinessSeqService.class}))//过滤掉不需要的类
 @EnableScheduling//定时器
 @EnableCaching//缓存
+@EnableDubboConfiguration
+@ServletComponentScan
 public class SpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
