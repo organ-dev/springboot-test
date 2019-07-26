66c08013d463f0dfb61cad06eba113fb3d1b3146
tutu <30116992@qq.com>
2019-07-26 18:24:33 +0800

    支付异步回调接口

--- src/main/java/com/example/controller/WatchCallBackController.java
+++ src/main/java/com/example/controller/WatchCallBackController.java
@@ -2,12 +2,16 @@ package com.example.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.utils.watchPay.WXPayUtil;
+import com.example.utils.watchUtil.Res;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -148,8 +152,87 @@ public class WatchCallBackController {
 	 * @auther: LiuDong
 	 * @date: 11:00 2019/7/26
 	 */
-	@RequestMapping(value = "/watchPayCallBack")
-	public void watchPayCallBack() {
+	@RequestMapping(value = "/watchPayNotify")
+	public Res watchPayCallBack(HttpServletRequest request) {
+		String result;//返回给微信的处理结果
+		String inputLine;
+		String notityXml = "";
+		try {
+			request.setCharacterEncoding("UTF-8");
+			while ((inputLine = request.getReader().readLine()) != null) {
+				notityXml += inputLine;
+			}
+			request.getReader().close();
+			logger.info("微信支付回调接口 data:{}" + JSONObject.toJSONString(notityXml));
+			Map map = WXPayUtil.xmlToMap(notityXml);
+			// 解析各种数据
+			String appid = (String) map.get("appid");//应用ID
+			String attach = (String) map.get("attach");//商家数据包
+			String bank_type = (String) map.get("bank_type");//付款银行
+			String cash_fee = (String) map.get("cash_fee");//现金支付金额
+			String fee_type = (String) map.get("fee_type");//货币种类
+			String is_subscribe = (String) map.get("is_subscribe");//是否关注公众账号
+			String mch_id = (String) map.get("mch_id");//商户号
+			String nonce_str = (String) map.get("nonce_str");//随机字符串
+			String openid = (String) map.get("openid");//用户标识
+			String out_trade_no = (String) map.get("out_trade_no");// 获取商户订单号
+			String result_code = (String) map.get("result_code");// 业务结果
+			String return_code = (String) map.get("return_code");// SUCCESS/FAIL
+			String sign = (String) map.get("sign");// 获取签名
+			String time_end = (String) map.get("time_end");//支付完成时间
+			String total_fee = (String) map.get("total_fee");// 获取订单金额
+			String trade_type = (String) map.get("trade_type");//交易类型
+			String transaction_id = (String) map.get("transaction_id");//微信支付订单号
 
+			if (("SUCCESS").equals(result_code)) {
+				Map msg = new HashMap<String, String>();
+				msg.put("return_code", "SUCCESS");
+				msg.put("return_msg", "OK");
+				result = WXPayUtil.mapToXml(msg);
+			} else {
+				Map msg = new HashMap<String, String>();
+				msg.put("return_code", "fail");
+				msg.put("return_msg", "微信返回的交易状态不正确（result_code=" + result_code + "）");
+				result = WXPayUtil.mapToXml(msg);
+				logger.error("微信返回的交易状态不正确（result_code=" + result_code + "）");
+			}
+			// 如果微信返回的结果是success，则修改订单状态
+			if (("SUCCESS").equals(return_code)) {
+				Map msg = new HashMap<String, String>();
+				msg.put("return_code", "SUCCESS");
+				msg.put("return_msg", "OK");
+				result = WXPayUtil.mapToXml(msg);
+				//TODO 修改流水信息、修改订单信息、修改商户金额、调用用户信息修改接口
+
+
+			} else {
+				Map msg = new HashMap<String, String>();
+				msg.put("return_code", "fail");
+				msg.put("return_msg", return_code);
+				result = WXPayUtil.mapToXml(msg);
+			}
+			logger.info("微信支付回调接口,返回给微信的xml：" + result);
+			return Res.Success(result);
+		} catch (Exception e) {
+			logger.error("message:" + e.getMessage());
+			Map msg = new HashMap<String, String>();
+			msg.put("return_code", "fail");
+			msg.put("return_msg", "xml获取失败");
+			try {
+				result = WXPayUtil.mapToXml(msg);
+			} catch (Exception e1) {
+			}
+		}
+		if (StringUtils.isEmpty(notityXml)) {
+			Map msg = new HashMap<String, String>();
+			msg.put("return_code", "fail");
+			msg.put("return_msg", "xml为空");
+			try {
+				result = WXPayUtil.mapToXml(msg);
+			} catch (Exception e) {
+			}
+		}
+		return Res.Failure(Res.Failure, "订单支付失败！");
 	}
+
 }
